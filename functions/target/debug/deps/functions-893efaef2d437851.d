6f70871c9aabcbdb46dd1303eebe6c80e1321db9
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2019-06-12 11:21:28 -0400

    Basic functions in rust.

---
+++ functions/target/debug/deps/functions-893efaef2d437851.d
@@ -0,0 +1,5 @@
+/home/vennila/projects/rust_programming/functions/target/debug/deps/functions-893efaef2d437851: src/main.rs
+
+/home/vennila/projects/rust_programming/functions/target/debug/deps/functions-893efaef2d437851.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/vennila/projects/rust_programming/functions/target/debug/functions: /home/vennila/projects/rust_programming/functions/src/main.rs
