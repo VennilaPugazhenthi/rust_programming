4f8d1cd364bca9805cb960b5eaedee6633a6ee78
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2019-06-12 11:25:39 -0400

    Basic variables in rust

---
+++ variables/target/debug/deps/variables-ff918a73d637a73c.d
@@ -0,0 +1,5 @@
+/home/vennila/projects/rust_programming/variables/target/debug/deps/variables-ff918a73d637a73c: src/main.rs
+
+/home/vennila/projects/rust_programming/variables/target/debug/deps/variables-ff918a73d637a73c.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/vennila/projects/rust_programming/variables/target/debug/variables: /home/vennila/projects/rust_programming/variables/src/main.rs
